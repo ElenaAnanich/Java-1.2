8c15dedd35b072d872b17e2c2893da0dd1adc150
Lena k <ktyecz09@mail.ru>
2023-01-07 23:22:38 +0300

    gitignore added

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int priceticket = 1000; //цена билета
-        int milesprice = 20; // стоимость 1 мили
-        int milles = priceticket / milesprice; // колличество миль  полученных по 1 билету
+        int priceTicket = 1000; //цена билета
+        int milesPrice = 20; // стоимость 1 мили
+        int milles = priceTicket / milesPrice; // колличество миль  полученных по 1 билету
 
         System.out.println((milles) + " Колличество полученых миль за покупку билета");
     }
